2aab7eba9133b51035e07d99a7c8126e588b2e1e
Jack Arthur Harrhy <me@jackharrhy.com>
2019-06-27 01:27:48 -0230

    initial project

---
+++ README.md
@@ -0,0 +1,20 @@
+jjar
+====
+
+[![CircleCI](https://circleci.com/gh/jackharrhy/jjar.svg?style=svg)](https://circleci.com/gh/jackharrhy/jjar)
+
+packs up json in a jar
+
+Install
+-------
+
+Via Cargo
+
+```sh
+cargo install jjar
+```
+
+Usage
+-----
+
+_coming soon_
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::fs;
+use std::io::{self, BufRead, BufReader};
+
+use serde_json::{Deserializer, Value};
+
+fn main() -> io::Result<()> {
+    let input = env::args().nth(1);;
+    let reader: Box<BufRead> = match input {
+        None => Box::new(BufReader::new(io::stdin())),
+        Some(filename) => Box::new(BufReader::new(fs::File::open(filename).unwrap())),
+    };
+
+    let stream = Deserializer::from_reader(reader).into_iter::<Value>();
+
+    let mut out = Vec::new();
+
+    for value in stream {
+        match value.ok() {
+            Some(value) => out.push(value),
+            None => (),
+        }
+    }
+
+    match serde_json::to_string_pretty(&out).ok() {
+        Some(string) => {
+            println!("{}", string);
+            Ok(())
+        },
+        None => Ok(())
+    }
+}
